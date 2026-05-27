3de8d70a662b1dc99f6be7323580d27f7e358e16
sheehan <onism1767@gmail.com>
2026-05-26 17:08:59 -0700

    Add SQL query for conversion funnel analysis

---
+++ sql/03_conversion_funnel.sql
@@ -0,0 +1,21 @@
+SELECT
+  event_name,
+  COUNT(*) AS event_count,
+  COUNT(DISTINCT user_pseudo_id) AS users
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE event_name IN (
+  'session_start',
+  'view_item',
+  'add_to_cart',
+  'begin_checkout',
+  'purchase'
+)
+GROUP BY event_name
+ORDER BY
+  CASE event_name
+    WHEN 'session_start' THEN 1
+    WHEN 'view_item' THEN 2
+    WHEN 'add_to_cart' THEN 3
+    WHEN 'begin_checkout' THEN 4
+    WHEN 'purchase' THEN 5
+  END;
